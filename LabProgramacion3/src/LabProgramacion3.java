62bb69f619592b907e8ec73afe819cdea34244d0
Brayan3000750 <brayanpinel030@gmail.com>
2024-10-25 09:51:11 -0600

    Ponemos un system.out para poner el menu y lo hacemos con swtich para que se pueda ejecutar cualquier opcion que pida el usuario

---
+++ LabProgramacion3/src/LabProgramacion3.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labprogramacion3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class LabProgramacion3 {
+
+    public static void main(String[] args) {
+        Scanner datos =new Scanner(System.in);
+   
+    
+    
+    
+    
+    }
+    
+}
---
+++ LabProgramacion3/src/Menu.java
@@ -0,0 +1,78 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class Menu {
+    public static void main(String[] args){
+    
+    Scanner datos=new Scanner(System.in);
+    int opciones;
+    
+    System.out.println("\nMenú");
+            System.out.println("1. Cadenas");
+            System.out.println("2. Notas");
+            System.out.println("3. Clases");
+            System.out.println("4. Salir");
+            System.out.println("Seleccione una de las opciónes: ");
+            opciones = datos.nextInt();
+            datos.nextLine();
+    
+    
+    switch(opciones){
+    
+        case 1:
+        System.out.println("Ingrese la cadena");
+        String cadenas=datos.nextLine();
+       
+        int[] contar=new int[256];
+        
+        for(int x=0;x<cadenas.length();x++){
+            contar[cadenas.charAt(x)]++;
+        }
+        
+          char caracterMaximo = ' ';
+                    int repeticionesMaximas = 0;
+
+                    for (int i = 0; i < contar.length; i++) {
+                        if (contar[i] > repeticionesMaximas) {
+                            repeticionesMaximas = contar[i];
+                            caracterMaximo = (char) i;
+                        }
+                    }
+   System.out.println("El carácter con mas repeticiones es: " + caracterMaximo + " con " + repeticionesMaximas + " repeticiones.");
+                    break;
+                    
+        case 2:
+            int cantNotas;
+            do{
+            System.out.println("Ingrese la cantidad de notas que quiera");    
+            cantNotas=datos.nextInt();
+            if (cantidadNotas <= 0) {
+               System.out.println("La cantidad debe ser positiva.");
+            }
+                    
+                    
+                    
+                    
+                    
+                    }
+    }
+    }
+
+        
+
+    
+    
+    
+
+    
+    
+
